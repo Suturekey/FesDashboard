00f6d74a7c0e0be0e7dc74125a653523658d5c56
Suturekey <razaman.to1@googlemail.com>
2025-01-18 19:43:04 +0100

    Added main chart

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,18 +1,76 @@
 <script setup lang="ts">
+import { onMounted, useTemplateRef, watch } from "vue";
 import AthleteCard from "../components/AthleteCard.vue";
-
 import { useAthleteStore } from "../stores/athleteStore";
+import {
+  Chart,
+  LineController,
+  LineElement,
+  PointElement,
+  LinearScale,
+  CategoryScale,
+  Title,
+} from "chart.js";
+
+const canvas = useTemplateRef("speed-canvas");
+
+const labels = [...Array(60)].map((_, idx) => `-${idx}s`);
+
+let speedChart: Chart;
+
+onMounted(() => {
+  Chart.register(
+    LineController,
+    LineElement,
+    PointElement,
+    LinearScale,
+    CategoryScale,
+    Title
+  );
+
+  speedChart = new Chart(canvas.value!, {
+    type: "line",
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: "Something",
+          data: [1, 2, 10],
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+      scales: {
+        x: {
+          reverse: true,
+        },
+      },
+    },
+  });
+});
 
 const athleteStore = useAthleteStore();
+
+watch(
+  () => athleteStore.athleteList,
+  () => {
+    speedChart.data.datasets.forEach((dataset) => {
+      dataset.data.push(1);
+    });
+    speedChart.update();
+  }
+);
 </script>
 
 <template>
   <div>
+    <canvas ref="speed-canvas"></canvas>
     <div class="athleteList">
       <AthleteCard
         v-for="athlete in athleteStore.athleteList"
         :athlete="athlete"
-        :stats="athleteStore.athleteMap.get(athlete.athleteId)"
+        :stats="athleteStore.athleteAnalysis.get(athlete.athleteId)"
         @click="$router.push(`/athlete/${athlete.athleteId}`)"
       ></AthleteCard>
     </div>
